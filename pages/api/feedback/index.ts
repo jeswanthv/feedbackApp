0be89a24ec5dce941ca17fd732b012499493f000
jeswanthv <jeswanthv001@gmail.com>
2022-05-22 13:37:41 +0530

    error fix

--- pages/api/feedback/index.ts
+++ pages/api/feedback/index.ts
@@ -1,12 +1,13 @@
 // pages/api/post/index.ts
 
+import { NextApiHandler } from 'next';
 import { getSession } from 'next-auth/react';
-import {prisma} from '../../../lib/prisma';
+import { prisma } from '../../../lib/prisma';
 
 // POST /api/post
 // Required fields in body: title
 // Optional fields in body: content
-export default async function handle(req, res) {
+const handle: NextApiHandler = async (req, res) => {
   const { title, content } = req.body;
 
   const session = await getSession({ req });
@@ -18,4 +19,5 @@ export default async function handle(req, res) {
     },
   });
   res.json(result);
-}
+};
+export default handle;
