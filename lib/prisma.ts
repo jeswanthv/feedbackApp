18300a63824c722ed84868af1bc58c208350aa22
jeswanthv <jeswanthv001@gmail.com>
2022-05-22 13:22:46 +0530

    error fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,18 @@
+// lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
+// let prisma: PrismaClient;
 declare global {
-  // allow global `var` declarations
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
-const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log: ['query'],
-  });
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
 export default prisma;
